2520210512b65b7e55f6195054dc9e94eaa2ea82
nmaguiar <nmaguiar@users.noreply.github.com>
2026-01-30 02:26:28 +0000

    remove debug files

--- write_table.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-printf "| File | Author |\n|---|---|\n"
-for f in $(find mcps -type f); do
-  a=$(grep -i -m1 -E "author[:=]" "$f" | sed -E "s/.*[Aa]uthor[:=]\\s*//")
-  [ -z "$a" ] && a="N/A"
-  printf "| %s | %s |\n" "$f" "$a"
-done > result.md
